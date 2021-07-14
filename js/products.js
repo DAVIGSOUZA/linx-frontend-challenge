21b5d648b687e13d1e168d7a737f00896795a3b8
Davi Garcia <davigs@hotmail.com>
2021-07-14 11:53:02 -0300

    add products scripts documentation

--- js/products.js
+++ js/products.js
@@ -1,7 +1,33 @@
+// FORMAT A NUMBER TO LOCAL CURRENCY
+// 
+// params: 
+//  Number - required
+//
+// returns:
+//  string
 const toLocalCurrency = (value) => {
   return `R$${value.toFixed(2).replace('.', ',')}`
 }
 
+// RECEIVES AN ARRAY OF PRODUCTS AND RENDER EACH PRODUCT 
+//
+// params: 
+//  array of objects
+//  object schema :
+//    {
+//      name,
+//      image,
+//      description,
+//      oldPrice,
+//      price,
+//      installments {
+//        count,
+//        value
+//      }
+//    }
+//
+// returns:
+//  void
 const renderProducts = (products) => {
   const productCards = products.map(product => `
     <div class="product-card">
@@ -24,8 +50,16 @@ const renderProducts = (products) => {
   document.getElementById('product-cards-container').innerHTML += productCards
 }
 
+// API PAGE VARIABLE
 let apiPage = 1
 
+// FETCH PRODUCTS FROM SPECIFIC PAGE FROM API
+// 
+// params: 
+//  Number - required
+//
+// returns:
+//  Promise<object>
 async function getData(page) {
   const data = await fetch(`
   https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=${page}
@@ -33,10 +67,15 @@ async function getData(page) {
   return data.json()
 }
 
+// GET PRODUCTS FROM API, RENDER IT, UPDATE API PAGE VARIABLE FOR NEW REQUESTS
+//
+// returns:
+//  void
 async function showProducts() {
   let data = await getData(apiPage)
   renderProducts(data.products)
   apiPage++
 }
 
+// INITICIAL PRODUCT RENDER
 showProducts()
